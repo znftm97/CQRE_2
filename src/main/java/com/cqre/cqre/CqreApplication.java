0b38570373d09c8c8bcac60d5ddb99b5010fa1ef
jihoon <znftm97@naver.com>
2021-01-17 19:58:48 +0900

    프로젝트 생성

---
+++ src/main/java/com/cqre/cqre/CqreApplication.java
@@ -0,0 +1,13 @@
+package com.cqre.cqre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cqre/cqre/CqreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cqre.cqre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
