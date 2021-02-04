f01af37addb76d2cb8a1c92cbc929efac827b5f7
jihoon <znftm97@naver.com>
2021-02-04 21:14:27 +0900

    OAuth2 로그인 - 네이버,페이스북,카카오 구현

--- src/main/java/com/cqre/cqre/security/OAuth2/OAuthAttributes.java
+++ src/main/java/com/cqre/cqre/security/OAuth2/OAuthAttributes.java
@@ -22,6 +22,14 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }else if("facebook".equals(registrationId)){
+            return ofFacebook(userNameAttributeName, attributes);
+        } else if ("kakao".equals(registrationId)) {
+            return ofKakao("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -30,6 +38,43 @@ public class OAuthAttributes {
         return OAuthAttributes.builder()
                 .name((String) attributes.get("name"))
                 .email((String) attributes.get("email"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofFacebook(String userNameAttributeName,
+                                            Map<String, Object> attributes) {
+
+        return OAuthAttributes.builder()
+                .name((String) attributes.get("name"))
+                .email((String) attributes.get("email"))
+                .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName,
+                                            Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofKakao(String userNameAttributeName,
+                                           Map<String, Object> attributes) {
+
+        Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+        Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");
+
+        return OAuthAttributes.builder()
+                .name(properties.get("nickname").toString())
+                .email(kakao_account.get("email").toString())
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
