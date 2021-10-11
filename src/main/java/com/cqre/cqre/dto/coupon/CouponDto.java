c95e6047b42ef617080e1ee3b20bfbb7d81a055a
jihoon <znftm97@naver.com>
2021-10-11 19:51:01 +0900

    쿠폰 생성 값 검증(Validation) 로직을 서비스 -> 컨트롤러 계층으로 이동, 이펙티브 자바 아이템6 내용 활용

--- src/main/java/com/cqre/cqre/dto/coupon/CouponDto.java
+++ src/main/java/com/cqre/cqre/dto/coupon/CouponDto.java
@@ -1,18 +1,36 @@
 package com.cqre.cqre.dto.coupon;
 
 import com.cqre.cqre.domain.shop.Coupon;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 public class CouponDto {
 
     private Long id;
+
+    @NotEmpty(message = "필수로 입력해야 합니다.")
     private String name;
-    private Long  discountRate;
+
+    @NotNull(message = "필수로 입력해야 합니다.")
+    @Max(message = "할인율은 100 미만이여야 합니다.", value = 99)
+    @Min(message = "할인율은 0 초과여야 합니다.", value = 1)
+    private Long discountRate;
+
     private LocalDateTime createDate;
-    private int totalCount;
+
+    @NotNull(message = "필수로 입력해야 합니다.")
+    @Max(message = "수량은 1000 미만이여야 합니다.", value = 999)
+    @Min(message = "수량은 0 초과여야 합니다.", value = 1)
+    private Integer totalCount;
+
     private int remainCount;
 
     public CouponDto(Coupon coupon) {
