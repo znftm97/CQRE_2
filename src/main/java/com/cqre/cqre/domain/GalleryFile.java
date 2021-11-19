311c065d56ececff558133e039eb523b2f0cc653
jihoon <znftm97@naver.com>
2021-11-19 20:20:32 +0900

    엔티티 정리 - GalleryFile

--- src/main/java/com/cqre/cqre/domain/GalleryFile.java
+++ src/main/java/com/cqre/cqre/domain/GalleryFile.java
@@ -1,14 +1,15 @@
 package com.cqre.cqre.domain;
 
 import com.cqre.cqre.domain.user.User;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
-@Builder
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class GalleryFile extends BaseEntity{
 
@@ -31,4 +32,15 @@ public class GalleryFile extends BaseEntity{
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    @Builder
+    public GalleryFile(String title, String originFilename, String filename, String filePath, String bundleId, Long bundleOrder, User user) {
+        this.title = title;
+        this.originFilename = originFilename;
+        this.filename = filename;
+        this.filePath = filePath;
+        this.bundleId = bundleId;
+        this.bundleOrder = bundleOrder;
+        this.user = user;
+    }
 }
