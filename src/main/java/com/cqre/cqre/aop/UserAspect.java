035edb7a3780a573aa9c926fea52617b98dd377a
jihoon <znftm97@naver.com>
2022-01-02 23:26:33 +0900

    Spring AOP 적용 - 소셜 로그인시 로깅

--- src/main/java/com/cqre/cqre/aop/UserAspect.java
+++ src/main/java/com/cqre/cqre/aop/UserAspect.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +17,9 @@ public class UserAspect {
     @Pointcut("execution(* com.cqre.cqre.security.CUserDetailsService.loadUserByUsername(String))")
     private void login(){}
 
+    @Pointcut("execution(* com.cqre.cqre.security.OAuth2.CustomOAuth2UserService.loadUser(..))")
+    private void socialLogin(){}
+
     @Around("execution(* com.cqre.cqre.controller.homeController.home())")
     public Object connectLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("============= connect user =============");
@@ -23,8 +27,14 @@ public class UserAspect {
     }
 
     @Before("login() && args(loginId,..)")
-    public void loginLog2(String loginId){
+    public void loginLog(String loginId){
         log.info("============= login user - " + loginId + " =============");
+    }
+
+    @Around("socialLogin() && args(req,..)")
+    public Object socialLoginLog(ProceedingJoinPoint joinPoint, OAuth2UserRequest req) throws Throwable {
+        log.info("============= social login - " + req.getClientRegistration().getRegistrationId() + " =============");
+        return joinPoint.proceed();
     }
 
 }
